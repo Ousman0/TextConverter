eb4d75af0f937bbf476ee87eed46ddfd880edf88
Ousman0 <ousmanmohammad23@gmail.com>
2023-05-03 16:12:23 +0530

    updated

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function Alert(props) {
   return (
-    props.alert&&<div className={`alert alert-${props.alert.typ} alert-dismissible fade show`} role="alert">
+    <div style={{height:"10px"}}>
+
+    {props.alert&&<div className={`alert alert-${props.alert.typ} alert-dismissible fade show`} role="alert">
     <strong>{props.alert.msg}</strong>: {props.alert.typ}
    
+    </div>}
   </div>
   )
 }
